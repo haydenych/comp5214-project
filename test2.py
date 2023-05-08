da19ff20b2dd238702253154af29aba78458d694
arnoldtsui <tsuiarnold@gmail.com>
2023-05-09 03:20:32 +0800

    final check

---
+++ test2.py
@@ -0,0 +1,135 @@
+from diffusers import UNet2DModel, DDPMScheduler, DDPMPipeline
+import torch
+from PIL import Image
+import torch.nn.functional as F
+from diffusers.optimization import get_cosine_schedule_with_warmup
+from torch.utils.data import DataLoader
+import math
+import os
+from utils import *
+from accelerate import Accelerator
+from huggingface_hub import HfFolder, Repository, whoami
+from tqdm.auto import tqdm
+from pathlib import Path
+from dataset.Vimeo90k import Vimeo90k
+from omegaconf import OmegaConf
+from taming.models import vqgan
+
+config = get_config()
+
+model = UNet2DModel(
+    sample_size=config.image_size,  # the target image resolution
+    in_channels=256*3,  # the number of input channels, 3 for RGB images
+    out_channels=256,  # the number of output channels
+    layers_per_block=2,  # how many ResNet layers to use per UNet block
+    #block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channels for each UNet block
+    block_out_channels=(256, 256, 512, 512),
+    down_block_types=(
+        # "DownBlock2D",  # a regular ResNet downsampling block
+        # "DownBlock2D",
+        "DownBlock2D",
+        "DownBlock2D",
+        "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+        "DownBlock2D",
+    ),
+    up_block_types=(
+        "UpBlock2D",  # a regular ResNet upsampling block
+        "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+        "UpBlock2D",
+        "UpBlock2D",
+        # "UpBlock2D",
+        # "UpBlock2D",
+    ),
+)
+
+noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
+timesteps = torch.LongTensor([50])
+
+auto_config = OmegaConf.load("config/16384_model.yaml")
+autoEncoder = vqgan.VQModel(**config.model.params)
+autoEncoder.init_from_ckpt("ckpts/16384_last.ckpt").to(device)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_data = Vimeo90k(path_to_dataset='../data/vimeo_triplet/sequences', datalist_filename='../data/vimeo_triplet/tri_trainlist.txt')
+train_dataloader = DataLoader(dataset = train_data, batch_size=4, shuffle=True)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
+lr_scheduler = get_cosine_schedule_with_warmup(
+    optimizer=optimizer,
+    num_warmup_steps=config.lr_warmup_steps,
+    num_training_steps=(len(train_dataloader) * config.num_epochs),
+)
+
+def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
+    accelerator = Accelerator(
+        mixed_precision=config.mixed_precision,
+        gradient_accumulation_steps=config.gradient_accumulation_steps,
+        log_with="tensorboard",
+        logging_dir=os.path.join(config.output_dir, "logs"),
+    )
+    if accelerator.is_main_process:
+        if config.output_dir is not None:
+            os.makedirs(config.output_dir, exist_ok=True)
+        accelerator.init_trackers("train_example")
+        
+    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+        model, optimizer, train_dataloader, lr_scheduler
+    )
+    
+    global_step = 0
+    
+    for epoch in range(config.num_epochs):
+        progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
+        progress_bar.set_description(f"Epoch {epoch}")
+        
+        for step, batch in enumerate(train_dataloader):
+            img1 = batch[:,0].squeeze().to(device)
+            img2 = batch[:,1].squeeze().to(device)
+            img3 = batch[:,2].squeeze().to(device)
+            
+            img1_code = model.encode(img1)
+            img2_code = model.encode(img2)
+            img3_code = model.encode(img3)
+            
+            noise = torch.randn(img2_code.shape).to(device)
+            bs = img2_code.shape[0]
+            
+            timesteps = torch.randint(
+                0, noise_scheduler.config.num_train_timesteps, (bs,), device=device
+            ).long()
+            
+            noisy_images = noise_scheduler.add_noise(img2_code, noise, timesteps)
+            
+            with accelerator.accumulate(model):
+                # Predict the noise residual
+                noise_pred = model(torch.cat(img1_code,noisy_images,img3_code), timesteps, return_dict=False)[0]
+                loss = F.mse_loss(noise_pred, noise)
+                accelerator.backward(loss)
+
+                accelerator.clip_grad_norm_(model.parameters(), 1.0)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+                
+            progress_bar.update(1)
+            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
+            progress_bar.set_postfix(**logs)
+            accelerator.log(logs, step=global_step)
+            global_step += 1
+
+        if accelerator.is_main_process:
+            pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
+
+            # if (epoch + 1) % config.save_image_epochs == 0 or epoch == config.num_epochs - 1:
+            #     evaluate(config, epoch, pipeline)
+
+            if (epoch + 1) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
+                pipeline.save_pretrained(config.output_dir)
+                print("saved", flush=True)
+    
+from accelerate import notebook_launcher
+
+args = (config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
+
+notebook_launcher(train_loop, args, num_processes=1)
--- utils.py
+++ utils.py
@@ -1,6 +1,33 @@
 from torchvision.transforms import transforms
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+import torch
+from dataclasses import dataclass
+import os
+
+@dataclass
+class TrainingConfig:
+    image_size = 128  # the generated image resolution
+    train_batch_size = 16
+    eval_batch_size = 16  # how many images to sample during evaluation
+    num_epochs = 50
+    gradient_accumulation_steps = 1
+    learning_rate = 1e-4
+    lr_warmup_steps = 500
+    save_image_epochs = 1
+    save_model_epochs = 1
+    #mixed_precision = "fp16"  # `no` for float32, `fp16` for automatic mixed precision
+    mixed_precision = "no"
+    output_dir = "output"  # the model name locally and on the HF Hub
+
+    push_to_hub = False  # whether to upload the saved model to the HF Hub
+    hub_private_repo = False
+    overwrite_output_dir = True  # overwrite the old model when re-running the notebook
+    seed = 0
+    
+def get_config():
+    return TrainingConfig
 
 def image_ten2pil(image):
     reverse_transforms = transforms.Compose([
@@ -16,3 +43,28 @@ def image_ten2pil(image):
     if len(image.shape) == 4:
         image = image[0, :, :, :] 
     return reverse_transforms(image)
+
+def make_grid(images, rows, cols):
+    w, h = images[0].size
+    grid = Image.new("RGB", size=(cols * w, rows * h))
+    for i, image in enumerate(images):
+        grid.paste(image, box=(i % cols * w, i // cols * h))
+    return grid
+
+def evaluate(config, epoch, pipeline):
+    # Sample some images from random noise (this is the backward diffusion process).
+    # The default pipeline output type is `List[PIL.Image]`
+    images = pipeline(
+        batch_size=config.eval_batch_size,
+        generator=torch.manual_seed(config.seed),
+    ).images
+
+    # Make a grid out of the images
+    image_grid = make_grid(images, rows=4, cols=4)
+
+    # Save the images
+    test_dir = os.path.join(config.output_dir, "samples")
+    os.makedirs(test_dir, exist_ok=True)
+    image_grid.save(f"{test_dir}/{epoch:04d}.png")
+    
+    
